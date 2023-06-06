bde0891d20db39152fa3a9cd6efb8d1ec13248da
ganjisriver <ganjisriver18@gmail.com>
2023-06-06 18:32:54 +0900

    Adjust General Login ID Encryption

--- BE/blur/blur-auth/src/main/java/com/blur/auth/api/service/UserService.java
+++ BE/blur/blur-auth/src/main/java/com/blur/auth/api/service/UserService.java
@@ -5,6 +5,7 @@ import com.blur.auth.api.entity.User;
 import com.blur.auth.api.dto.Role;
 import com.blur.auth.api.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -12,20 +13,21 @@ import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class UserService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
 
-    public void register(UserSignUpDto UserSignUpDto) throws Exception {
-        if (userRepository.findById(UserSignUpDto.getEmail()).isPresent()) {
+    public void register(UserSignUpDto userSignUpDto) throws Exception {
+        log.info("유저이메일 들어오는지 확인", userSignUpDto.getEmail() );
+        if (userRepository.findById(userSignUpDto.getEmail()).isPresent()) {
             throw new Exception("이미 존재하는 이메일");
         }
 
         User user = User.builder()
                 .role(Role.GUEST)
-                .id(UserSignUpDto.getEmail())
-                .password(UserSignUpDto.getPassword())
-                .id(UUID.randomUUID().toString())
+                .id(userSignUpDto.getEmail())
+                .password(userSignUpDto.getPassword())
                 .build();
 
         user.passwordEncode(passwordEncoder);
