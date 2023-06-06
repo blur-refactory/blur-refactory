7b0516921b7150c05d0b38ff938cef0ce2ea5de8
김창겸 <kyum8562@naver.com>
2023-06-06 23:19:28 +0900

    :zap: 2319
    2319

--- FE/src/components/Chat/chatsocket.jsx
+++ FE/src/components/Chat/chatsocket.jsx
@@ -1,7 +1,7 @@
 // socket.jsx
-import socketIOClient from "socket.io-client";
+// import socketIOClient from "socket.io-client";
 
-const ENDPOINT = "http://blurblur.kr/user-service/ws";
-const chatsocket = socketIOClient(ENDPOINT);
+// const ENDPOINT = "http://blurblur.kr/user-service/ws";
+// const chatsocket = socketIOClient(ENDPOINT);
 
-export default chatsocket;
+// export default chatsocket;
