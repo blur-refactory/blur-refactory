558fc2ea8e3753e9a7a74cb36a8a8e005c2d3cce
ganjisriver <ganjisriver18@gmail.com>
2023-06-15 16:47:06 +0900

    Fix changePassword & SecurityConfig antMatchers

--- BE/blur/blur-auth/src/main/java/com/blur/auth/config/SecurityConfig.java
+++ BE/blur/blur-auth/src/main/java/com/blur/auth/config/SecurityConfig.java
@@ -59,9 +59,8 @@ public class SecurityConfig {
 
                 //== URL별 권한 관리 옵션 ==//
                 .authorizeRequests()
-
-//                .antMatchers( "/ws/**", "/socket.io/**", "/auth/**", "/api/login/**", "/login/**")
-                .antMatchers( "/**")
+                .antMatchers( "/ws/**", "/socket.io/**", "/auth/**", "/api/login/**", "/login/**")
+//                .antMatchers( "/**")
                 .permitAll()
                 .anyRequest()
                 .authenticated() // 위의 경로 이외에는 모두 인증된 사용자만 접근 가능
