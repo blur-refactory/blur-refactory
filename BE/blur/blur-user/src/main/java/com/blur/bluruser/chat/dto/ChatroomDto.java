57c652a9c57046491d4e370ce0c24bec741ef069
최동호 <ehdrmfdl002@naver.com>
2023-07-15 16:49:24 +0900

    :art: modify chatroomDtoList

---
+++ BE/blur/blur-user/src/main/java/com/blur/bluruser/chat/dto/ChatroomDto.java
@@ -0,0 +1,22 @@
+package com.blur.bluruser.chat.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ChatroomDto {
+    private String id;
+
+    private String MyName;
+
+    private String OpponentName;
+
+    private String MyId;
+
+    private String OpponentId;
+}
--- BE/blur/blur-user/src/main/java/com/blur/bluruser/chat/service/ChatroomService.java
+++ BE/blur/blur-user/src/main/java/com/blur/bluruser/chat/service/ChatroomService.java
@@ -1,11 +1,11 @@
 package com.blur.bluruser.chat.service;
 
+import com.blur.bluruser.chat.dto.ChatroomDto;
 import com.blur.bluruser.chat.dto.MakeChatroomDto;
-import com.blur.bluruser.chat.entity.Chatroom;
 
 import java.util.List;
 
 public interface ChatroomService {
     void makeChatroom(MakeChatroomDto makeChatroomDto);
-    List<Chatroom> getChatroom(String email);
+    List<ChatroomDto> getChatroom(String email);
 }
